394be6fc57a9a14c4586b1f17c1f389e545255e1
RalatPC <ralatrado@gmail.com>
2022-09-24 16:43:37 +0100

    removed single item from array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+function removeFromArray(array, arg) {
+    for (let i = 0; i <= array.length - 1; i++) {
+        if (array[i] === arg) {
+        array.splice(i, 1);
+        }
+    }
+   return array;
+}
 
-};
+removeFromArray([1, 2, 3, 4], 3);
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
