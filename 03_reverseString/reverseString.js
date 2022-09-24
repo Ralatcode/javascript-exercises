6136b67f96fd25d09faa2a1f4007ec980c5e7484
RalatPC <ralatrado@gmail.com>
2022-09-24 09:58:34 +0100

    exercise 3 test 1

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
-const reverseString = function() {
+function reverseString(str) {
+    let splitString = str.split('');
+    let reverseArray = splitString.reverse();
+    let joinArray = reverseArray.join('');
 
+    return joinArray;
 };
+
+// test 1
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
