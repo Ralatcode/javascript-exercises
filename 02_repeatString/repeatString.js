379e0be9c0d2c6727459cd4e435923e41476ce5e
RalatPC <ralatrado@gmail.com>
2022-09-24 08:52:48 +0100

    exercise 2 test 1

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,14 @@
-const repeatString = function() {
 
+function repeatString (string, num) {
+    let result = '';    
+    for (let i = 0; i < num; i++) {
+        result += string;
+    }
+
+    return result;    
 };
+
+repeatString('hey', 3);
 
 // Do not edit below this line
 module.exports = repeatString;
