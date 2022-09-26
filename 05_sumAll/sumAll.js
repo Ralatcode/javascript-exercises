446318caf527fa728b5e9f6f065aa06fd670afd0
RalatPC <ralatrado@gmail.com>
2022-09-26 18:31:24 +0100

    sumAll test

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,21 @@
-const sumAll = function() {
+function sumAll(min, max) {
+    if (!Number.isInteger(min) || !Number.isInteger(max)) return "ERROR";
+    if (min < 0 || max < 0) return "ERROR";
+    if (min > max) {
+      const temp = min;
+      min = max;
+      max = temp;
+    }
+    let sum = 0;
+    for (let i = min; i < max + 1; i++) {
+      sum += i;
+    }
+    return sum;
+  };
+// const result = sumAll(1,4);
 
-};
+// console.log(result);
+
 
 // Do not edit below this line
 module.exports = sumAll;
