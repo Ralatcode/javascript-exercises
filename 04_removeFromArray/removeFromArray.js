e0b65fdf9eb84d3cb5bd38ee30d0e215b7f277b3
RalatPC <ralatrado@gmail.com>
2022-09-26 10:05:04 +0100

    pass test 2 and 3

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -10,13 +10,9 @@ function removeFromArray(array, ...args) {
 
 }
 
-// for (let i = 0; i <= array.length - 1; i++) {
-    //         if (array[i] === arg) {
-    //         array.splice(i, 1);
-    //         }
-    //     }
-    //    return array
 removeFromArray([1, 2, 3, 4], 3);
+removeFromArray([1, 2, 3, 4], 3, 2);
+removeFromArray([1, 2, 3, 4], "tacos");
 
 
 
