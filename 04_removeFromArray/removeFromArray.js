8f65a61fee8bd8876901c3fb198def0a6f00337b
RalatPC <ralatrado@gmail.com>
2022-09-26 09:48:01 +0100

    took another approach

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,12 +1,21 @@
-function removeFromArray(array, arg) {
-    for (let i = 0; i <= array.length - 1; i++) {
-        if (array[i] === arg) {
-        array.splice(i, 1);
+function removeFromArray(array, ...args) {
+    args.forEach((arg) => {
+        const index = array.indexOf(arg);
+        if (index >= 0) {
+            array.splice(index, 1);
         }
-    }
-   return array;
+    });
+
+    return array;
+
 }
 
+// for (let i = 0; i <= array.length - 1; i++) {
+    //         if (array[i] === arg) {
+    //         array.splice(i, 1);
+    //         }
+    //     }
+    //    return array
 removeFromArray([1, 2, 3, 4], 3);
 
 
