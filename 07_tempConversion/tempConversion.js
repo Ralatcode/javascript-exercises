48aecc007c200279acefb9750908c29cea1b15c3
RalatPC <ralatrado@gmail.com>
2022-09-27 16:27:55 +0100

    pass test for temp Conversion task

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(f) {
+  let result = (f - 32) * (5 / 9);
+  let finalResult = Math.round(result * 10) / 10;
+  return finalResult;
 };
 
-const ctof = function() {
-
+const ctof = function(c) {
+  let result = c * (9 / 5) + 32;
+  let finalResult = Math.round(result * 10) / 10;
+  return finalResult;
 };
 
 // Do not edit below this line
