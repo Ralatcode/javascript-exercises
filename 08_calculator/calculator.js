30d85209305b53a7a2cc9f4656450cf9086e0a7d
RalatPC <ralatrado@gmail.com>
2022-10-09 00:22:16 +0100

    completed calculator test

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,48 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  let sumValue = 0;
+	for (num of array) {
+    sumValue += num;
+  }
+  return sumValue;
 };
 
-const multiply = function() {
+const multiply = function(arr) {
+  let multiplyValue = 0;
 
+  if (arr.length >= 1) {
+    multiplyValue = 1;
+  }
+  for (num of arr) {
+    multiplyValue *= num;
+  }
+
+  return multiplyValue;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	if (num === 0) {
+    return 1;
+  }
+
+  let result = 1; 
+  for (let i = num; i > 0; i--) {
+    result *= i; 
+  }
+  return result;
 };
+
 
 // Do not edit below this line
 module.exports = {
