0e03da765e36bcb0ed1fa589f5c5f0c52ba26359
RalatPC <ralatrado@gmail.com>
2022-10-09 10:53:34 +0100

    completed get oldest person task

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    const currentYear = new Date().getFullYear();
+    const oldestPerson = arr.sort((a, b) => {
+        const person1 = (a.yearOfDeath || currentYear) - a.yearOfBirth;
+        const person2 = (b.yearOfDeath || currentYear) - b.yearOfBirth;
+        return person1 > person2 ? -1 : 1;
+    })
+
+    return oldestPerson[0]
 
 };
 
