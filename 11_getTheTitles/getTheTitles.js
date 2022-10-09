58114ee3fce082b5f68eb9221ed5ecc69409acf7
RalatPC <ralatrado@gmail.com>
2022-10-09 02:01:00 +0100

    completed the getTitle task

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,6 @@
-const getTheTitles = function() {
-
+const getTheTitles = function(array) {
+    const titleArr = array.map((book) => book.title);
+    return titleArr;
 };
 
 // Do not edit below this line
