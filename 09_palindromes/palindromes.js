fdd0909e9d56f715c57c4b5215a4bf3eed790f92
RalatPC <ralatrado@gmail.com>
2022-10-09 01:27:52 +0100

    completed palindrome test

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,24 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    let splitStr = str.toLowerCase().split('');
+    const punctStrip = splitStr.filter((char) => {
+        if (char === ' ' || char === ',' || char === '!' || char === '.') {
+            return false;
+        } else {
+            return true;
+        }
+    });
 
+    const newStr = punctStrip.join('');
+    const reverseStr = punctStrip.reverse().join('');
+
+    if (newStr === reverseStr) {
+        return true;
+    } else {
+        return false;
+    }
 };
+
+
 
 // Do not edit below this line
 module.exports = palindromes;
